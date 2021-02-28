e0dad08920d29534dd56409cf513a1454400fa73
meetmanjusri <meetmanjusri@gmail.com>
2021-02-28 01:44:13 -0600

    Additional Changes

--- gallery/models.py
+++ gallery/models.py
@@ -14,6 +14,16 @@ class Artist(models.Model):
 class Collection(models.Model):
     collection_name = models.CharField(max_length=50)
     collection_description = models.CharField(max_length=280)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return str(self.collection_name)
