3e09520034dcb10428489f02c7b9035d19a543b8
meetmanjusri <meetmanjusri@gmail.com>
2021-02-28 10:34:06 -0600

    Model and Admin Changes

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist, Artwork, Favorite, Collection, Rating
+
+
+class ArtistList(admin.ModelAdmin):
+    list_display = ('user_name', 'artist_description')
+    list_filter = ('user_name', 'artist_description')
+    search_fields = ('user_name',)
+    ordering = ['user_name']
+
+
+class ArtworkList(admin.ModelAdmin):
+    list_display = ('artist_name', 'artwork_title', 'artwork_description')
+    list_filter = ('artist_name', 'artwork_title')
+    search_fields = ('artist_name',)
+    ordering = ['artist_name']
+
+
+class FavoriteList(admin.ModelAdmin):
+    list_display = ('user_name', 'artwork_name')
+    list_filter = ('user_name', 'artwork_name')
+    search_fields = ('user_name',)
+    ordering = ['user_name']
+
+
+class CollectionList(admin.ModelAdmin):
+    list_display = ('user_name', 'collection_name', 'collection_description')
+    list_filter = ('user_name', 'collection_name')
+    search_fields = ('user_name',)
+    ordering = ['user_name']
+
+
+class RatingList(admin.ModelAdmin):
+    list_display = ('user_name', 'rating_level', 'artwork_name')
+    list_filter = ('user_name', 'artwork_name')
+    search_fields = ('user_name',)
+    ordering = ['user_name']
+
+
+admin.site.register(Artist, ArtistList)
+admin.site.register(Artwork, ArtworkList)
+admin.site.register(Favorite, FavoriteList)
+admin.site.register(Collection, CollectionList)
+admin.site.register(Rating, RatingList)
--- gallery/models.py
+++ gallery/models.py
@@ -4,29 +4,12 @@ from django.contrib.auth.models import User
 
 
 class Artist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Reference - https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+    user_name = models.OneToOneField(User, on_delete=models.CASCADE)
     artist_description = models.CharField(max_length=280)
 
     def __str__(self):
-        return str(self.user)
-
-
-class Collection(models.Model):
-    collection_name = models.CharField(max_length=50)
-    collection_description = models.CharField(max_length=280)
-    created_date = models.DateTimeField(default=timezone.now)
-    updated_date = models.DateTimeField(auto_now_add=True)
-
-    def created(self):
-        self.created_date = timezone.now()
-        self.save()
-
-    def updated(self):
-        self.updated_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return str(self.collection_name)
+        return str(self.user_name)
 
 
 class Artwork(models.Model):
@@ -45,12 +28,29 @@ class Artwork(models.Model):
         self.save()
 
     def __str__(self):
-        return str(self.artist_name)
+        return str(self.artwork_title)
 
 
-class Tag(models.Model):
-    tag_name = models.CharField(max_length=50)
-    tag_description = models.CharField(max_length=280)
+class Favorite(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    artwork_name = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+
+class Collection(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    collection_name = models.CharField(max_length=50)
+    collection_description = models.CharField(max_length=280)
+    artworks = models.ManyToManyField(Artwork, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(auto_now_add=True)
 
@@ -63,11 +63,31 @@ class Tag(models.Model):
         self.save()
 
     def __str__(self):
-        return str(self.tag_name)
+        return str(self.collection_name)
+
+
+# class Tag(models.Model):
+#     tag_name = models.CharField(max_length=50)
+#     tag_description = models.CharField(max_length=280)
+#     created_date = models.DateTimeField(default=timezone.now)
+#     updated_date = models.DateTimeField(auto_now_add=True)
+#
+#     def created(self):
+#         self.created_date = timezone.now()
+#         self.save()
+#
+#     def updated(self):
+#         self.updated_date = timezone.now()
+#         self.save()
+#
+#     def __str__(self):
+#         return str(self.tag_name)
 
 
 class Rating(models.Model):
     rating_level = models.IntegerField(default=5)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    artwork_name = models.ForeignKey(Artwork, on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(auto_now_add=True)
 
